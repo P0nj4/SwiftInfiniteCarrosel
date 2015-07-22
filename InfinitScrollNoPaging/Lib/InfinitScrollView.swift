e67b77c43bf8c31fdc78e3ab52545ba960034c02
German Pereyra <german.f.pereyra@gmail.com>
2015-07-22 17:03:24 -0300

    Start

---
+++ InfinitScrollNoPaging/Lib/InfinitScrollView.swift
@@ -0,0 +1,115 @@
+//
+//  InfinitScrollView.swift
+//  InfinitScrollNoPaging
+//
+//  Created by German Pereyra on 7/22/15.
+//  Copyright (c) 2015 German Pereyra. All rights reserved.
+//
+
+import UIKit
+
+
+protocol InfinitScrollViewDelegate {
+    func viewForItem(item: AnyObject, inFrame: CGRect) -> UIView
+    func didSelectItem(item: AnyObject)
+}
+
+class InfiniteScrollItemView: UIView {
+    var object:AnyObject!
+    var innerView:UIView!
+    init(frame: CGRect, object: AnyObject, innerView: UIView) {
+        super.init(frame: frame)
+        self.innerView = innerView;
+        self.object = object;
+        self.addSubview(innerView)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class InfinitScrollView: UIView {
+    let scroll : UIScrollView = UIScrollView()
+    private var datasource : Array<AnyObject>!
+    private var itemsCount : Int!
+    private var itemWidth : Float!
+    private var startingPoint : CGFloat!
+    private var endingPoint : CGFloat!
+    var delegate: InfinitScrollViewDelegate!
+    
+    init(frame: CGRect, datasource: Array<AnyObject>, itemWidth: Float, delegate: InfinitScrollViewDelegate) {
+        super.init(frame: frame)
+        self.datasource = datasource;
+        self.itemWidth = itemWidth;
+        self.itemsCount = self.datasource.count;
+        self.startingPoint = CGFloat(self.itemWidth * Float(self.itemsCount));
+        self.endingPoint = CGFloat((self.itemWidth * Float(self.itemsCount)) * Float(2));
+        self.delegate = delegate;
+        scroll.frame = self.bounds
+        scroll.delegate = self;
+        self.initialize()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+ 
+    func initialize() {
+        scroll.delegate = self
+        self.scroll.contentSize = CGSizeMake(CGFloat(self.itemWidth * Float(self.itemsCount) * 3), CGFloat(self.scroll.bounds.size.height))
+        self.scroll.contentOffset = CGPointMake(self.startingPoint, CGFloat(0))
+        self.addSubview(scroll)
+        self.renderizeScroll()
+    }
+    
+    func renderizeScroll () {
+        var leftPosX = CGFloat(0);
+        for (var j = 0; j < 3; j++) {
+            for (var i = 0; i < self.itemsCount; i++) {
+                var frame = CGRect(x: leftPosX, y: 0, width: CGFloat(self.itemWidth), height: self.scroll.bounds.size.height)
+                var innerView = self.delegate.viewForItem(self.datasource[i], inFrame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
+                var itemView = InfiniteScrollItemView(frame: frame, object: self.datasource[i], innerView: innerView)
+                leftPosX += CGFloat(self.itemWidth)
+                
+                var button = UIButton(frame: itemView.bounds)
+                button .addTarget(self, action:"itemSelectionEvent:", forControlEvents:UIControlEvents.TouchUpInside)
+                itemView .addSubview(button)
+                
+                self.scroll.addSubview(itemView)
+            }
+        }
+    }
+    
+    func itemSelectionEvent(sender:UIButton!) {
+        if let itemView = sender.superview as? InfiniteScrollItemView {
+            self.delegate .didSelectItem(itemView.object)
+        }
+    }
+}
+
+extension InfinitScrollView : UIScrollViewDelegate {
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        self.scroll.userInteractionEnabled = true
+        if (self.scroll.contentOffset.x > self.endingPoint) {
+            self.scroll.contentOffset = CGPoint(x: self.scroll.contentOffset.x - CGFloat(self.itemWidth * Float(self.itemsCount)), y: 0)
+        }
+        if (self.scroll.contentOffset.x < self.startingPoint) {
+            self.scroll.contentOffset = CGPoint(x: self.scroll.contentOffset.x + CGFloat(self.itemWidth * Float(self.itemsCount)), y: 0)
+        }
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        if !scrollView.decelerating {
+            self.scroll.userInteractionEnabled = true
+        }
+    }
+    
+    func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        print("isDecelerating: \(scrollView.decelerating) velocity: \(velocity)")
+        if velocity.x != 0 {
+            //self.scroll.userInteractionEnabled = false
+        }
+    }
+    
+}
--- InfinitScrollNoPaging/ViewController.swift
+++ InfinitScrollNoPaging/ViewController.swift
@@ -12,14 +12,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var array = ["A", "B", "C", "D", "E", "F", "G"]
+        var infiniteView = InfinitScrollView(frame: CGRect(x: 0, y: 80, width: self.view.bounds.width, height: 300), datasource: array, itemWidth: 320, delegate: self)
+        self.view .addSubview(infiniteView)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+extension ViewController: InfinitScrollViewDelegate {
+    
+    func viewForItem(item: AnyObject, inFrame: CGRect) -> UIView {
+        var aView = UILabel(frame: inFrame)
+        aView.backgroundColor = self.getRandomColor()
+        aView.font = UIFont.systemFontOfSize(27)
+        aView.textColor = UIColor.whiteColor()
+        aView.textAlignment = NSTextAlignment.Center
+        aView.text = "\(item)"
+        return aView
+    }
+    
+    func getRandomColor() -> UIColor {
+        var randomRed:CGFloat = CGFloat(drand48())
+        var randomGreen:CGFloat = CGFloat(drand48())
+        var randomBlue:CGFloat = CGFloat(drand48())
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    }
+    
+    func didSelectItem(item: AnyObject) {
+        
+    }
+}
+
 
